fc21cf426e620dc3ddd698788e0abfb395e1b1c2
Falconii <falconi.2050@gmail.com>
2022-03-10 17:26:41 -0300

    110 - novo teste

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,12 +2,12 @@
 export const environment = {
   production: true,
   ambiente: 'Produção',
-  apiURL: 'http://192.168.0.161:63999/api/',
+  apiURL: 'https://192.168.0.161:63999/api/',
 };
 */
 
 export const environment = {
   production: true,
-  ambiente: 'Produção X',
-  apiURL: 'http://192.168.0.161:63999/api/',
+  ambiente: 'Produção OK',
+  apiURL: 'https://192.168.0.161:63999/api/',
 };
