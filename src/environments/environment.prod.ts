f1c6e0fb60c2f33efede06f3c227d03240cbc040
Falconii <falconi.2050@gmail.com>
2022-03-11 10:22:15 -0300

    FASE 1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,6 +8,6 @@ export const environment = {
 
 export const environment = {
   production: true,
-  ambiente: 'Produção OK',
-  apiURL: 'https://192.168.0.161:63999/api/',
+  ambiente: 'Produção FASE 1',
+  apiURL: 'https://localhost:3000/api/',
 };
